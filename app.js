6d8977a450253668f92ef42a3fa4a736d4e70910
blainedeyoung <45204685+blainedeyoung@users.noreply.github.com>
2021-05-12 22:46:51 -0400

    added a script to solve a firefox bug that sometimes prevents it from displaying favicons

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 /*
+  This script solves a firefox bug that sometimes stops it from displaying favicons
+*/
+
+function setFavicon() {
+  var link = $('link[type="image/x-icon"]').remove().attr("href");
+  $('<link href="'+ link +'" rel="shortcut icon" type="image/x-icon" />').appendTo('head');
+}
+
+/*
 11.5 seconds after the window loads this will
 add a hover to the grass and trees image which will
 make the information panel drop down
